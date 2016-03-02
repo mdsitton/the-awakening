b1dc27d2f00186793a4a0e0a66d37f1776177656
Matthew Sitton <matthewsitton@gmail.com>
2016-03-02 00:06:59 -0600

    Switch event handlers to override the methods in the window class. Add resize to game.py Add mouse movement event to game.py.

--- theawakening/engine.py
+++ theawakening/engine.py
@@ -5,53 +5,47 @@ class Engine(object):
     def __init__(self):
         self.title = "The Awakening"
 
-        self.window = window.Window(800, 600, resizable=True,
-                caption=self.title, fullscreen=False)
-
         self.listeners = []
         self.cursor_rel = False
 
         self.runFunc = lambda dt: None
 
-        @self.window.event
-        def on_resize(width, height):
-            self.broadcast_event('resize', (width, height))
+        # These classes need to access variables contained within Engine
+        # Without really changing how they function to much. Defining them
+        # in the init of Engine is the simplest way to do this.
+        class MyWindow(window.Window):
+            def on_resize(wself, width, height):
+                self.broadcast_event('resize', (width, height))
 
-        @self.window.event
-        def on_key_release(symbol, modifiers):
-            self.broadcast_event('key_up', (symbol, modifiers))
+            def on_key_release(wself, symbol, modifiers):
+                self.broadcast_event('key_up', (symbol, modifiers))
 
-        @self.window.event
-        def on_key_press(symbol, modifiers):
-            self.broadcast_event('key_down', (symbol, modifiers))
+            def on_key_press(wself, symbol, modifiers):
+                self.broadcast_event('key_down', (symbol, modifiers))
 
-        @self.window.event
-        def on_mouse_motion(x, y, dx, dy):
-            if self.is_cursor_relative():
-                data = (dx, dy)
-            else:
-                data = (x, y)
-            self.broadcast_event('mouse_move', data)
+            def on_mouse_motion(wself, x, y, dx, dy):
+                if self.is_cursor_relative():
+                    data = (dx, dy)
+                else:
+                    data = (x, y)
+                self.broadcast_event('mouse_move', data)
 
-        @self.window.event
-        def on_mouse_press(x, y, button, modifiers):
-            self.broadcast_event('mouse_down', (button, modifiers))
+            def on_mouse_press(wself, x, y, button, modifiers):
 
-        @self.window.event
-        def on_mouse_release(x, y, button, modifiers):
-            self.broadcast_event('mouse_up', (button, modifiers))
+                self.broadcast_event('mouse_move', (x, y))
+                self.broadcast_event('mouse_down', (button, modifiers))
 
-        @self.window.event
-        def on_mouse_scroll(x, y, dx, dy):
-            self.broadcast_event('mouse_scroll', (x, y, dx, dy))
+            def on_mouse_release(wself, x, y, button, modifiers):
+                self.broadcast_event('mouse_up', (button, modifiers))
 
-        @self.window.event
-        def on_close():
-            self.broadcast_event('on_close', None)
+            def on_mouse_scroll(wself, x, y, dx, dy):
+                self.broadcast_event('mouse_scroll', (x, y, dx, dy))
 
-        @self.window.event
-        def on_window_close(window):
-            self.broadcast_event('window_close', window)
+            def on_close(wself):
+                self.broadcast_event('on_close', None)
+
+            def on_window_close(wself, window):
+                self.broadcast_event('window_close', window)
 
         class NewEventLoop(pyglet.app.EventLoop):
             def idle(inself):
@@ -61,6 +55,9 @@ class Engine(object):
                 self.runFunc(dt)
 
                 return 0
+
+        self.window = MyWindow(800, 600, resizable=True,
+                caption=self.title, fullscreen=False)
 
         self.eventLoop = NewEventLoop()
 
--- theawakening/game.py
+++ theawakening/game.py
@@ -13,15 +13,36 @@ class Game(object):
         playerImg = pyglet.image.load('data/player.png')
         self.player = pyglet.sprite.Sprite(playerImg)
 
+        self.mouseX = 0
+        self.mouseY = 0
+
         self.keys = []
 
     def process_events(self, event, data):
-        if event == 'on_close':
-            self.engine.stop()
+        if event == 'mouse_move':
+            if self.engine.is_cursor_relative():
+                self.mouseRelX = data[0]
+                self.mouseRelY = data[1]
+            else:
+                self.mouseX = data[0]
+                self.mouseY = data[1]
+
         elif event == 'key_down':
             self.keys.append(data[0])
         elif event == 'key_up':
             self.keys.remove(data[0])
+        elif event == 'resize':
+            width, height = data
+            self.resize(width, height)
+        elif event == 'on_close':
+            self.engine.stop()
+
+    def resize(self, width, height):
+        gl.glViewport(0, 0, width, height)
+        gl.glMatrixMode(gl.GL_PROJECTION)
+        gl.glLoadIdentity()
+        gl.glOrtho(0, width, 0, height, -1, 1)
+        gl.glMatrixMode(gl.GL_MODELVIEW)
 
     def update(self, dt):
         if pyglet.window.key.W in self.keys:
@@ -32,7 +53,7 @@ class Game(object):
             self.player.y -= 50 * dt
         if pyglet.window.key.D in self.keys:
             self.player.x += 50 * dt
-        print (self.keys)
+        print (self.mouseX, self.mouseY)
 
     def render(self):
         self.engine.window.switch_to()
